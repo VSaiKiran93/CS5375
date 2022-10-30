fc30977b85f1997008002eae44df06efdca3fca8
VSaiKiran93 <82889124+VSaiKiran93@users.noreply.github.com>
2022-10-29 23:35:52 -0500

    Update Part_1.c

--- Part_1.c
+++ Part_1.c
@@ -65,8 +65,8 @@ int main(int argc, char *argv[])
         printf("==================================\n");
         printf("Cache Hits:    %d\n", d_cache.hits);
         printf("Cache Misses:  %d\n", d_cache.misses);
-        printf("Cache Hit rate: %0.3f%%\n", Cache_hitrate);
-        printf("Cache Miss rate: %0.3f%%\n", Cache_missrate);
+        printf("Cache Hit rate: %0.3f%%\n", Cache_hitrate*100);
+        printf("Cache Miss rate: %0.3f%%\n", Cache_missrate*100);
         printf("\n");
     }
 
